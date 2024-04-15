16ad4e6303522a15cb2c706ce9150ebf97dac2c9
ashifanassar <ashifanassar@gmail.com>
2024-04-15 17:48:44 +0530

    terraform code

---
+++ 02-variables/output.tf
@@ -0,0 +1,18 @@
+#variable "sample" {}    //Declaring the empty variable
+
+
+variable "sample" {
+    default = "terraform"
+}
+
+output "sample_op" {
+    value = var.sample
+}
+
+output "sample_output_var" {
+    value = "value of sample is ${var.sample}"
+}
+
+output "batch serial" {
+    value = var.batch
+}
--- 02-variables/vars.tf
+++ 02-variables/vars.tf
@@ -1,14 +1,8 @@
-#variable "sample" {}    //Declaring the empty variable
-
-
 variable "sample" {
     default = "terraform"
 }
 
-output "sample_op" {
-    value = var.sample
-}
 
-output "sample_output_var" {
-    value = "value of sample is ${var.sample}"
+variable "batch" {
+  default = 57
 }
