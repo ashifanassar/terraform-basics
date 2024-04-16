2e45dbb4b0a12d32339e684bef0c997630775741
ashifanassar <ashifanassar@gmail.com>
2024-04-16 19:27:41 +0530

    terraform basics

--- 05-remote-backend-with-DynamoDB/ec2.tf
+++ 05-remote-backend-with-DynamoDB/ec2.tf
@@ -14,3 +14,30 @@ resource "aws_instance" "app" {
 output "private_ip_address" {
     value = aws_instance.app.private_ip
 }
+
+
+#security group
+
+resource "aws_security_group" "allow_tls" {
+  name        = "devops_security_group"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+    ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+ egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+     Name = "allow_ssh_sg"
+  }
+
+}
+
