d788bac5f9512f99bbb35a4840382cdbddf724d1
ashifanassar <ashifanassar@gmail.com>
2024-04-16 19:41:45 +0530

    terraform basics

--- 05-remote-backend-with-DynamoDB/ec2.tf
+++ 05-remote-backend-with-DynamoDB/ec2.tf
@@ -1,8 +1,9 @@
 
 # Defining the resources 
 resource "aws_instance" "app" {
-  ami           = "ami-072983368f2a6eab5"
-  instance_type = "t3.micro"
+  ami                     = "ami-072983368f2a6eab5"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
 
 
   tags = {
