219bd0171eb7539cfedaa0306417d58b18f650f5
ashifanassar <ashifanassar@gmail.com>
2024-04-16 05:36:18 +0530

    terraform code

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -4,7 +4,7 @@
 
 resource "aws_instance" "web" {
   ami           = "ami-072983368f2a6eab5"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "terraform-instance-dev"
