d9d2d0167848a18bcfd38f84bed4cf58934c3281
ashifanassar <ashifanassar@gmail.com>
2024-04-17 04:25:39 +0530

    terraform basics

--- 05-remote-backend-with-DynamoDB/ec2.tf
+++ 05-remote-backend-with-DynamoDB/ec2.tf
@@ -24,8 +24,8 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic and all outbound traffic"
 
     ingress {
-    from_port       = 0
-    to_port         = 0
+    from_port       = 22
+    to_port         = 22
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
